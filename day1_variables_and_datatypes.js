679f882bb7b19e5359af9bd10d5bdd2008f758d7
Puneet Sharma <puneetsharma200332@gmail.com>
2024-07-15 23:36:19 +0530

    day1js

---
+++ day1_variables_and_datatypes.js
@@ -0,0 +1,48 @@
+// activity 1: variables declaration
+// task1
+ 
+var num = 2
+console.log(num); 
+
+// task2
+let str = "puneet"
+console.log(str);
+
+// activity 2: constant declation
+// task3
+const t_F  =   true
+console.log(t_F);
+
+// activity 3: data types
+// task 4
+let n1 = 2
+let stringlog = "my name is puneet"
+let n2 = 233.43
+let bools = false
+let obj = {
+    name : "puneet",
+    age:  21,
+    salary: 100000
+}
+let arr = [123 , "puneet" , 2332.323]
+console.log(n1 +" is "+ typeof(n1));
+console.log(stringlog +" is "+ typeof(stringlog));
+console.log(n2 +" is "+ typeof(n2));
+console.log(bools +" is "+ typeof(bools));
+console.log(obj +" is "+ typeof(obj));
+console.log(arr +" is "+ typeof(arr));
+
+// activity 4
+// task 5
+ n1 = 3
+ console.log(n1);
+
+//  activity 5
+// task 6
+ t_F = false
+//  console.log(t_F); this will give error
+
+
+
+    
+ 
