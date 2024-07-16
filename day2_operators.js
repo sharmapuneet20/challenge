268cabb3d56d25107714aa358f289009038a0a3d
Puneet Sharma <puneetsharma200332@gmail.com>
2024-07-16 17:35:15 +0530

    day2

---
+++ day2_operators.js
@@ -0,0 +1,42 @@
+// activity 1  arthmetic operations
+
+// task 1  ,2 ,3 , 4 ,  5
+let a = 10
+let b =  3 
+console.log(a+b);
+console.log(a-b)
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+// activity 2 assigment operators
+// task 6 , 7
+   a +=2
+   b -=1
+//    console.log(a);
+//    console.log(b);
+//    activity 3 comparison operators
+// task 8 ,9, 10
+//  console.log(a<b);
+//  console.log(a>b);
+//  console.log(a>=b+" "+ a<=b);
+//  console.log(a==b);
+//  console.log(a===b);
+//  activity 4 logical operators
+// task 11 , 12, 13
+ console.log(a>=b && b<=a);
+ console.log(a<b || a==b);
+ console.log(a!=b);
+
+//  activty 5 ternary operator
+// task 14 
+ const age = 798
+ const beverage = age >=21? "beer" : "juice";
+ console.log(beverage);
+
+
+
+
+
+
+
