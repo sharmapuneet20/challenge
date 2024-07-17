6b8ce9bae5d962d1b2b3376a71c06bbab130e317
Puneet Sharma <puneetsharma200332@gmail.com>
2024-07-17 23:53:50 +0530

    day3

---
+++ day3_control.js
@@ -0,0 +1,41 @@
+// activity 1 
+// task 1
+
+let num1 = 5
+if(num1> 0){
+    console.log("positive");
+}else if(num1 ==0){
+    console.log("0");
+}else{
+    console.log("negative");
+}
+
+// task 2
+let age = 19
+if(age>= 18){
+    console.log("elgible to vote");
+}
+else{
+    console.log("not eligible to vote");
+}
+
+// activity 2 
+// task 3
+ 
+ let a  = 6
+ let b = 4
+ let c  = 10
+
+ if(a>b &&  a >c){
+    console.log("a largest");
+ }else if(b>a && b> c){
+    console.log("b largest");
+ }else{
+    console.log("c largest");
+ }
+//  activity 3 
+// task 4
+
+ 
+ 
+ 
