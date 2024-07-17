9470dd40e6929a8e2f07be055e6a7cbd86de98e6
Puneet Sharma <puneetsharma200332@gmail.com>
2024-07-18 00:24:02 +0530

    day3

--- day3_control.js
+++ day3_control.js
@@ -36,6 +36,71 @@ else{
 //  activity 3 
 // task 4
 
+ const days  = 5;
+ switch(days){
+    case 1: console.log("monday");
+    break;
+
+
+   case 2: console.log("tueday");
+   break;
+
+   case 3: console.log("wednesday");
+   break;
+
+   case 4: console.log("thursday");
+   break;
+   case 5: console.log("friday");
+   break;
+   case 6: console.log("saturday");
+   break;
+   case 7: console.log("sunday");
+   break;
+
+   default:
+    console.log("invalid day number");
+ }
+    
+ const scores = 99
+
+ switch (true) {
+    case(scores>=91 && scores<=100):
+{ console.log("A");
+        }
+        break;
+
+        case 
+         (scores>=81 && scores<=90) :{
+            console.log("B");
+        }
+        break;
+        case (scores>=71 && scores<=80): {
+            console.log("C");
+        }
+        break;
+        case  (scores>=61 && scores<=70): {
+            console.log("D");
+        }
+        break;
+    default:
+        console.log("invalid scores");
+        break;
+ }
+
+//  activity 4 
+//  task 6 
+let numb  =2
+   const ans  =  (numb%2 == 0)? "even" : " odd" ;
+   console.log(ans);
  
- 
+//   activity  5 
+//  task 7
+  
+const leapyear  = 2016
+
+if((leapyear % 4 == 0 && leapyear % 100 !== 0) || (leapyear % 400 ==0) ){
+    console.log("leapyear");
+}else{
+    console.log("not leapyear");
+}
  
