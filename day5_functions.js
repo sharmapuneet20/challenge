3f33fadc7743b1d7e888da6b8a8028227ecde90c
Puneet Sharma <puneetsharma200332@gmail.com>
2024-07-24 21:44:11 +0530

    day5

---
+++ day5_functions.js
@@ -0,0 +1,48 @@
+// activity 1
+// task 1
+
+  function numcheck(n) {
+    if(n%2== 0 ){
+        return true
+    }else{
+        return false;
+    }
+    
+  }
+
+//   console.log(numcheck(23));
+
+//   task 2 
+function square(n){
+    const ans  = n * n
+    return ans;
+}
+
+// console.log(square(10));
+
+//  activity 2 
+// task 3
+ function max( num1  , num2) {
+
+    if(num1 > num2){
+        console.log(num1);
+    }else{
+        console.log(num2);
+    }
+    
+ }
+
+//  max(12 , 10);
+
+// task 4 
+function concat(str , str1) {
+    const ans  = str + str1;
+     return ans;
+}
+
+// console.log(concat("puneet" , " sharma"));
+
+// activity 3 
+// task 5
+
+
