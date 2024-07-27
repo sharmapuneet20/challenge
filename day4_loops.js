008c19aa8d61e8fe06e0ea1d893d5817eec44274
Puneet Sharma <puneetsharma200332@gmail.com>
2024-07-27 22:21:25 +0530

    day4

--- day4_loops.js
+++ day4_loops.js
@@ -1,6 +1,39 @@
-// activity1
+activity1
 // task1
-for (let index = 1; index <= 10; index++) {
-    const element = array[index];
+for (let index = 0; index < 11; index++) {
+    const element = index;
     console.log(element);
 }
+// task 2
+ let a = 5;
+for (let index = 1; index <= 10; index++) {
+        const ele = a * index;
+        console.log(ele);
+    
+}
+
+// activity 2 
+//  task 3
+   let element =  0 ;
+for (let index = 1; index <=10; index++) {
+      element += index;
+}
+console.log(element);
+
+// task 4 
+for (let index = 10; index >=1; index--) {
+    const element = index;
+    console.log(element);
+}
+//  activity 3
+  
+//  task 5
+  let num  =1;
+ do{
+    console.log(num);
+    num++;
+
+ }while (num <= 5);
+
+//  task 6
+
